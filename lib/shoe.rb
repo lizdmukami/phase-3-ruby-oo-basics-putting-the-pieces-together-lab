30bf897367da3df632087f098a2e0f9e71ad8562
lizdmukami <lizdmukami@gmail.com>
2023-02-10 17:43:07 +0300

    Assignment completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,49 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def material
+        @material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        output = "Your shoe is as good as new!"
+        puts output
+        "says that the shoe has been repaired"
+        @condition = "new"
+    end
+end
+
+shoe = Shoe.new("Nike")
+puts shoe.cobble

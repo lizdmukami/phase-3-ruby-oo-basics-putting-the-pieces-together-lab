130e16519f6945d85bcfe12c637b53bfd7ed9926
lizdmukami <lizdmukami@gmail.com>
2023-02-10 17:42:29 +0300

    commit tree 17dbee770ee5fc131e786fc997f9941cf4c1958d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        "can turn a page"
+    end
+end
+
+book = Book.new("And Then There Were None")
+puts book.turn_page
